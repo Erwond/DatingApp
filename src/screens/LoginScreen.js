96bfdb33f8fc6dc7fafd75a97a3069dd778941ac
Walker Eric, GHR-SRT-NEX-E-10 <Eric.Walker@swisscom.com>
2019-05-27 23:12:49 +0200

    Login Button now gets Access and Refresh Tokens, as well as the expiration Date via Authorization Code

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, AsyncStorage } from 'react-native';
 import SpotifyButton from '../components/SpotifyButton';
 import { AuthSession } from 'expo';
 import { spotifyCredentials } from '../auth/secrets';
+import { encode as btoa } from 'base-64';
 
 class LoginScreen extends Component {
   state = {
@@ -19,7 +20,7 @@ class LoginScreen extends Component {
         {this.state.result ? (
           <Text>{JSON.stringify(this.state.result)}</Text>
         ) : null}
-        < SpotifyButton style={styles.loginButton} text="LOGIN WITH SPOTIFY" onPress={getAuthorizationCode} />
+        < SpotifyButton style={styles.loginButton} text="LOGIN WITH SPOTIFY" onPress={getTokens} />
       </View>
     );
   }
@@ -46,6 +47,96 @@ const getAuthorizationCode = async () => {
     return result.params.code;
   } catch (err) {
     console.error(err)
+  }
+}
+
+// Async Storage
+const saveAccessToken = async accessToken => {
+  try {
+    await AsyncStorage.setItem('accessToken', accessToken);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const getAccessToken = async () => {
+  let accessToken = '';
+  try {
+    accessToken = await AsyncStorage.getItem('accessToken') || 'none';
+  } catch (error) {
+    console.log(error.message);
+  }
+  return accessToken;
+}
+
+const saveRefreshToken = async refreshToken => {
+  try {
+    await AsyncStorage.setItem('refreshToken', refreshToken);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const getRefreshToken = async () => {
+  let refreshToken = '';
+  try {
+    refreshToken = await AsyncStorage.getItem('refreshToken') || 'none';
+  } catch (error) {
+    console.log(error.message);
+  }
+  return refreshToken;
+}
+
+const saveExpirationTime = async expirationTime => {
+  try {
+    await AsyncStorage.setItem('expirationTime', "" + expirationTime);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const getExpirationTime = async () => {
+  try {
+    let expiration = '';
+    expiration = await AsyncStorage.getItem('expirationTime') || 'none';
+  } catch (error) {
+    console.log(error.message);
+  }
+  return expiration;
+}
+
+
+// Get Tokens
+const getTokens = async () => {
+  try {
+    const authorizationCode = await getAuthorizationCode();
+    const credentials = spotifyCredentials;
+    const credsB64 = btoa(`${credentials.clientId}:${credentials.clientSecret}`);
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${credsB64}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `grant_type=authorization_code&code=${authorizationCode}&redirect_uri=${
+        credentials.redirectUri
+        }`,
+    });
+    const responseJson = await response.json();
+    // destructure the response and rename the properties to be in camelCase to satisfy my linter ;)
+    const {
+      access_token: accessToken,
+      refresh_token: refreshToken,
+      expires_in: expiresIn,
+    } = responseJson;
+
+    const expirationTime = new Date().getTime() + expiresIn * 1000;
+    await saveAccessToken(accessToken);
+    await saveRefreshToken(refreshToken);
+    await saveExpirationTime(expirationTime);
+
+  } catch (err) {
+    console.error(err);
   }
 }
 
